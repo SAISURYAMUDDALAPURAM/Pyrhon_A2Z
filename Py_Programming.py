470477c37bf10c62fb0164f50844517b55fba686
M SAI SURYA <61184618+SAISURYAMUDDALAPURAM@users.noreply.github.com>
2024-08-09 08:15:52 +0530

    Update Py_Programming.py

--- Py_Programming.py
+++ Py_Programming.py
@@ -1,1 +1,13 @@
+"""
+Python: Very Easy to Learn, Simple English Based Programming Language
+English : Grammer - In Speaking Language 
+Python  : Syntax  - In Programming Language (Due to  Free source & more Modulesvery easy and powerful and general purpose language)
 
+Python Full Stack:
+1)Core Python 
+2)Advanced Python
+3)Python Modules (CapStoneProj: Python Modules Project)
+4)UI (HTML,CSS,BS,JS,jQuery)
+5)MySQL
+6)Django & Flask (CapStoneProj: Python Full Stack Project)
+"""
